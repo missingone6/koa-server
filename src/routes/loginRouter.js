ea2877586b3a125ecb80cb6322365c2ecb8b0822
lyl <1106225065@qq.com>
2023-01-29 22:27:30 +0800

    fix: 修改登录注册路由

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -2,7 +2,7 @@ import Router from '@koa/router';
 import loginController from '../api/LoginController';
 const router = new Router();
 
-router.prefix('/api/users')
+router.prefix('/api/login')
 router.post('/login', loginController.login);
 router.post('/register', loginController.register);
 
